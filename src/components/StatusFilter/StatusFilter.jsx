35f576d9651e9e349705d8671910306dc5cfd5aa
Julia-Gavrashenko <julia.gavrashenko@gmail.com>
2023-04-09 20:57:45 +0300

    add-reducers

--- src/components/StatusFilter/StatusFilter.jsx
+++ src/components/StatusFilter/StatusFilter.jsx
@@ -3,15 +3,31 @@ import css from "./StatusFilter.module.css";
 import { useSelector } from "react-redux";
 import { statusFilters } from "redux/constants";
 import { getStatusfilter } from "redux/selectors";
+import { useDispatch } from "react-redux";
+import { setStatusFilter } from "redux/actions";
 
 export const StatusFilter = () => {
-const filter = useSelector(getStatusfilter)
+  
+  const dispatch = useDispatch();
+  const filter = useSelector(getStatusfilter)
+  
+  const handleFilterChange = filter => dispatch(setStatusFilter(filter))
+
 
   return (
     <div className={css.wrapper}>
-      <Button selected={filter === statusFilters.all}>All</Button>
-      <Button selected={filter === statusFilters.active}>Active</Button>
-      <Button selected={filter === statusFilters.completed}>Completed</Button>
+      <Button
+        selected={filter === statusFilters.all}
+        onClick={() => handleFilterChange(statusFilters.all)}
+      >All</Button>
+      <Button
+        selected={filter === statusFilters.active}
+        onClick={() => handleFilterChange(statusFilters.active)}
+      >Active</Button>
+      <Button
+        selected={filter === statusFilters.completed}
+        onClick={() => handleFilterChange(statusFilters.completed)}
+      >Completed</Button>
     </div>
   );
 };
